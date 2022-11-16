32aaa102ad375418d4ba7a561e6f80f550041869
dt <38030682+doanthanh274@users.noreply.github.com>
2022-11-16 18:59:17 +0800

    first commit

---
+++ zsh/wsl_firstboot.sh
@@ -0,0 +1,11 @@
+packages=(zsh docker)
+
+sudo -i
+apt update
+apt install $packages -y
+
+# installing zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo plugins=(zsh-autosuggestions zsh-syntax-highlighting git) >> ~/.zshrc
